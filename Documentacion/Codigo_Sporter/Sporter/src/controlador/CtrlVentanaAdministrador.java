f19e8d70c1dcbcc9f4d6f479bd82977f699db32f
Jose Luis Gonzalez <jlgonza07@gmail.com>
2020-06-03 19:01:52 +0200

    version v7

--- Documentacion/Codigo_Sporter/Sporter/src/controlador/CtrlVentanaAdministrador.java
+++ Documentacion/Codigo_Sporter/Sporter/src/controlador/CtrlVentanaAdministrador.java
@@ -4,50 +4,97 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.SQLException;
 
-import modelo.Deporte;
-import modelo.Persona;
+import javax.swing.JButton;
+
 import vistas.VentanaAdministrador;
+import vistas.VentanaPerfilUsuario;
 
 
+public class CtrlVentanaAdministrador implements ActionListener, MouseListener{
 
 	
-	public class CtrlVentanaAdministrador implements ActionListener, ItemListener {
+	private VentanaAdministrador vista;
+	
+	
+	
+	public CtrlVentanaAdministrador(VentanaAdministrador v){
+		super();
+		vista = v;
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String comando = e.getActionCommand();
 		
-		private VentanaAdministrador vista;
-		
-		public CtrlVentanaAdministrador(VentanaAdministrador v){
-			super();
-			vista = v;
-		}
-
-		// metodo que controla los eventos producidos por los botones de la ventana Administrador
-		public void actionPerformed(ActionEvent e) {
-			String comando = e.getActionCommand();
-			
-			if(comando.equals("ADMINISTRADOR") ) {
-				try {
-					vista.VentanaAdministrador();
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-			}else if (comando.contentEquals("CANCELAR")) {
-				vista.cerrarVentana();
+		switch (comando) {
+		case "PERFIL":
+			try {
+				vista.perfilUsuario();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-			
-		}
+			break;
 
-		@Override
-		public void itemStateChanged(ItemEvent e) {
-			// TODO Auto-generated method stub
-			
+		case "SALIR":
+			;
+			//vista.VentanaLogin();
+			break;
 		}
-
-		
 
 	}
 
 
+	@Override
+	public void mouseClicked(MouseEvent evt) {
+		int column = vista.tabla.getColumnModel().getColumnIndexAtX(evt.getX()); // posicion x de la columna cuando hacemos click
+		int row = evt.getY() / vista.tabla.getRowHeight(); // fila
+		
+		if(row < vista.tabla.getRowCount() && row >= 0 && column < vista.tabla.getColumnCount() && column >= 0) { // dentro del rango de la tabla
+			Object value = vista.tabla.getValueAt(row, column);
+			if(value instanceof JButton) {
+				((JButton) value).doClick();
+				JButton boton = (JButton) value;
+
+				if (boton.getName().equals("f")) {
+					try {
+						vista.filtrarEvento();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else if (boton.getName().equals("cs")) {
+					vista.iniciarSesion();
+				}
+			}
+		}
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub	
+	}
+}
